25eed7afa5389e9d9cc3306df24cfa4f1cbe53d5
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-06-05 20:53:26 -0300

    index add and save in deleteBtn value

--- scripts/functions/renderLibrary.js
+++ scripts/functions/renderLibrary.js
@@ -3,8 +3,10 @@ import { renderCardBook } from "./cardBook.js";
 export function renderLibrary(array) {
     const libraryDom = document.querySelector(`#libraryDOM`);
     libraryDom.innerHTML = ``;
+    let i = 0;
     
     array.forEach(element => {
-        libraryDom.appendChild(renderCardBook(element))
+        libraryDom.appendChild(renderCardBook(element, i))
+        i++;
     });
 }
