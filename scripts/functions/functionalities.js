a312adc6882b888981bf06919a904b86e4189e47
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-08-13 23:17:02 -0300

    standar js implement. And validateData function improved

--- scripts/functions/functionalities.js
+++ scripts/functions/functionalities.js
@@ -1,69 +1,68 @@
-import { Book } from "../prototypes/Book.js";
-import { renderLibrary } from "./renderLibrary.js";
+import { Book } from '../prototypes/Book.js'
+import { renderLibrary } from './renderLibrary.js'
 
-const bookTitle = document.querySelector(`#bookTitle`);
-const bookAuthor = document.querySelector(`#bookAuthor`);
-const bookpages = document.querySelector(`#bookpages`);
-const readStatus = document.querySelector(`#readStatus`);
+const bookTitle = document.querySelector('#bookTitle')
+const bookAuthor = document.querySelector('#bookAuthor')
+const bookpages = document.querySelector('#bookpages')
+const readStatus = document.querySelector('#readStatus')
 
-function validateData(){
-    if(bookTitle.value != `` && bookAuthor.value != `` && !Number.isNaN(Number(bookpages.value))) {
-        return true
-    }else {
-        alert("Data is missing, or perhaps you should make sure to put the number of pages in number format")
+function validateData () {
+  if (bookTitle.value !== '' && bookAuthor.value !== '' && !Number.isNaN(Number(bookpages.value)) && bookpages.value !== '' && !bookpages.value.includes(' ') && Number(bookpages.value) !== 0) {
+    return true
+  } else {
+    window.alert('Data is missing, or perhaps you should make sure to put the number of pages in number format')
+  }
+}
+// This function will be exported to each book card
+export function deleteBook (array, index) {
+  array.splice(index, 1)
+  renderLibrary(array)
+}
+
+export function changeReadStatus (array, index) {
+  const book = array[index]
+
+  book.isRead = !book.isRead
+  renderLibrary(array)
+}
+
+function resetInput () {
+  bookTitle.value = ''
+  bookAuthor.value = ''
+  bookpages.value = ''
+  readStatus.checked = false
+}
+
+function addBookToLibrary (array) {
+  const book = new Book(bookTitle.value, bookAuthor.value, Number(bookpages.value), readStatus.checked)
+
+  array.push(book)
+  resetInput()
+}
+
+function showElementFlex (element) {
+  element.style.display = 'flex'
+}
+
+function closeElement (element) {
+  element.style.display = 'none'
+  resetInput()
+}
+
+export function addFunctionalities (array) {
+  const modalContainer = document.querySelector('#modalContainer')
+
+  const addBookBtn = document.querySelector('.plusCard__btn')
+  const cancelBtn = document.querySelector('#cancelBtn')
+  const okBtn = document.querySelector('#okBtn')
+
+  addBookBtn.addEventListener('click', () => showElementFlex(modalContainer))
+  cancelBtn.addEventListener('click', () => closeElement(modalContainer))
+  okBtn.addEventListener('click', () => {
+    if (validateData()) {
+      addBookToLibrary(array)
+      closeElement(modalContainer)
+      renderLibrary(array)
     }
-}
-//This function will be exported to each book card
-export function deleteBook(array, index){
-    array.splice(index, 1);
-        renderLibrary(array);
-}
-
-export function changeReadStatus(array, index){
-    let book = array[index];
-
-    book.isRead = book.isRead ? false : true;
-        renderLibrary(array);
-}
-
-function resetInput(){
-    bookTitle.value = ``
-    bookAuthor.value = ``
-    bookpages.value = ``;
-    readStatus.checked = false;
-}
-
-function addBookToLibrary(array) {
-
-    const book = new Book(bookTitle.value,bookAuthor.value,Number(bookpages.value),readStatus.checked)
-
-    array.push(book);
-    resetInput();
-}
-
-function showElementFlex(element) {
-    element.style.display = "flex";
-}
-
-function closeElement(element) {
-    element.style.display = "none";
-    resetInput();
-}
-
-export function addFunctionalities(array) {
-    const modalContainer = document.querySelector(`#modalContainer`);
-
-    const addBookBtn = document.querySelector(`.plusCard__btn`);
-    const cancelBtn = document.querySelector(`#cancelBtn`);
-    const okBtn = document.querySelector(`#okBtn`);
-
-    addBookBtn.addEventListener(`click`, () => showElementFlex(modalContainer));
-    cancelBtn.addEventListener(`click`, () => closeElement(modalContainer));
-    okBtn.addEventListener(`click`, () => {
-        if(validateData()){
-            addBookToLibrary(array);
-            closeElement(modalContainer);
-            renderLibrary(array);
-        }
-    });
+  })
 }
